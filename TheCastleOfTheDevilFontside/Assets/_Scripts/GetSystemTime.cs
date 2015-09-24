d4f3c0f06e5b3427941efd1ba30c7829e35cc214
lagane100 <lagane100@gmail.com>
2015-09-24 23:53:54 +0800

    "basic pages & system time add"

---
+++ TheCastleOfTheDevilFontside/Assets/_Scripts/GetSystemTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GetSystemTime : MonoBehaviour {
+	public TextMesh text;
+	
+	// Update is called once per frame
+	void Update () {
+		text = (TextMesh)GetComponent ("TextMesh");
+		text.text = DateTime.Now.ToString("HH:mm");
+	}
+}
