72c6c3cb6eaa2b1460a1a5eac8d4d9b3d63c9c66
lagane100 <lagane100@gmail.com>
2015-09-26 18:40:16 +0800

    fail update with shining text

--- TheCastleOfTheDevilFontside/Assets/_Scripts/EnterMain.cs
+++ TheCastleOfTheDevilFontside/Assets/_Scripts/EnterMain.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 
 public class EnterMain : MonoBehaviour {
 	private TextMesh continueSign;
+	private Color[] textColor = new Color[4]{Color.clear, Color.gray, Color.black, Color.gray};
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +18,10 @@ public class EnterMain : MonoBehaviour {
 		if (Input.anyKeyDown) {
 			Application.LoadLevel(0);
 		}
+
+		//for (int i = 0; i < textColor.Length; i++) {
+		//	continueSign.color = textColor[i];
+		//	Thread.Sleep(200);
+		//}
 	}
 }
