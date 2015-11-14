177cd7fc5ce61a78f0c10853b6981687f2a6682f
lagane100 <lagane100@gmail.com>
2015-11-15 06:29:01 +0800

    add transfer

--- new_one_on_2D/Assets/_Script/loadingScreen.cs
+++ new_one_on_2D/Assets/_Script/loadingScreen.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class loadingScreen : MonoBehaviour {
 
+	public string levelToLoad;
+
+	public GameObject background;
+	public GameObject loading_bar;
+	public GameObject text;
+
+	private int loadProgress = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+		background.SetActive (false);
+		loading_bar.SetActive (false);
+		text.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown ("space")) {
+			StartCoroutine(DisplayLoadingScreen(levelToLoad));
+		}
+	}
+
+	// Display the loading screen while the scene is changing
+	IEnumerator DisplayLoadingScreen(string level){
+		background.SetActive (true);
+		loading_bar.SetActive (true);
+		text.SetActive (true);
+
+		loading_bar.transform.localScale = new Vector3 (loadProgress, loading_bar.transform.position.y, loading_bar.transform.position.z);
+
+		TextMesh textObject = GameObject.Find ("text").GetComponent<TextMesh> ();
+		textObject.text = "Now Loading " + loadProgress + "%";
+
+		AsyncOperation async = Application.LoadLevelAsync (level);
+		while (!async.isDone) {
+			loadProgress = (int)(async.progress * 100);
+			textObject.text = "Now Loading " + loadProgress + "%"; 
+			loading_bar.transform.localScale = new Vector3 (async.progress, loading_bar.transform.position.y, loading_bar.transform.position.z);
+			yield return null;
+		}
 	}
 }
