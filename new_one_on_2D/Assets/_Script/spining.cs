1722a787889e91d62e17fe5881282a87133de528
lagane100 <lagane100@gmail.com>
2015-11-15 10:26:13 +0800

    i don't know what i had add

---
+++ new_one_on_2D/Assets/_Script/spining.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spining : MonoBehaviour {
+	public int rotateAngle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (0, 0,rotateAngle * Time.deltaTime);	
+	}
+}
