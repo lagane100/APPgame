a525ede51c668ac2861199dfe71f9188ce444384
lagane100 <lagane100@gmail.com>
2015-11-22 16:12:28 +0800

    IMEI setting

---
+++ new_one_on_2D/Assets/_Script/Login_to_main.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login_to_main : MonoBehaviour {
+
+	private AndroidJavaObject TM = new AndroidJavaObject("android.telephony.TelephonyManager");
+
+
+	// Start is called once per script
+	void Start(){
+		string IMEI = TM.Call<string>("getDeviceId");
+	}
+
+	// Update is called once per frame
+	void Update () {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE
+		MouseInput();	//control by mouse
+#elif UNITY_ANDROID 
+		MobileInput(); //control by finger
+#endif
+	}
+	void MouseInput(){
+		if (Input.GetKeyDown("space")) {
+
+		}
+	}
+
+	void MobileInput(){
+
+	}	
+}
+
