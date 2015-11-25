491cf3311c2c9d439b7b93693fc1a65ecd266706
lagane100 <lagane100@gmail.com>
2015-11-25 15:08:18 +0800

    return

--- new_one_on_2D/Assets/_Script/Login_to_main.cs
+++ new_one_on_2D/Assets/_Script/Login_to_main.cs
@@ -3,27 +3,29 @@ using System.Collections;
 
 public class Login_to_main : MonoBehaviour {
 
-	private AndroidJavaObject TM = new AndroidJavaObject("android.telephony.TelephonyManager");
+	//private AndroidJavaObject TM = new AndroidJavaObject("android.telephony.TelephonyManager");
 
 
 	// Start is called once per script
 	void Start(){
-		string IMEI = TM.Call<string>("getDeviceId");
+		//string IMEI = TM.Call<string>("getDeviceId");
+		string IMEI = "359279065155027";
 	}
 
 	// Update is called once per frame
 	void Update () {
+
 //#if UNITY_EDITOR_OSX || UNITY_STANDALONE
 //		MouseInput();	//control by mouse
 //#elif UNITY_ANDROID 
 //		MobileInput(); //control by finger
 //#endif
-	}
+//	}
 //	void MouseInput(){
 //		if (Input.GetKeyDown("space")) {
 //		}
 //	}
 //	void MobileInput(){
-//	}	
+	}	
 }
 
