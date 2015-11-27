a32faad04e213a8bfadc41c20805c26d3e8ccb64
lagane100 <lagane100@gmail.com>
2015-11-27 21:57:36 +0800

    add a lot of things

--- new_one_on_2D/Assets/_Script/Server_reconnect.cs
+++ new_one_on_2D/Assets/_Script/Server_reconnect.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class Server_reconnect : MonoBehaviour {
+	private string things = "1234";
 
+	public GameObject Text;
+	public GameObject OK;
 	// Use this for initialization
 	void Start () {
-	
+		Text.SetActive (false);
+		OK.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown ("space")/*change to server connect error*/) {
+			StartCoroutine(reconnectToServer(things));
+		}
+	}
+
+	IEnumerator reconnectToServer(string thing_to_send){
+		Text.SetActive (true);
+		OK.SetActive (true);
+
+		yield return 0;
 	}
 }
