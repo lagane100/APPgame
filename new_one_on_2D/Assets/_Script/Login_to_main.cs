57e16ecfa9bce336f20a7566f95cfa73b6c28bb8
lagane100 <lagane100@gmail.com>
2015-11-29 18:23:52 +0800

    add minijson plugin

--- new_one_on_2D/Assets/_Script/Login_to_main.cs
+++ new_one_on_2D/Assets/_Script/Login_to_main.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using MiniJSON;
 
 public class Login_to_main : MonoBehaviour {
 
@@ -9,23 +12,42 @@ public class Login_to_main : MonoBehaviour {
 	// Start is called once per script
 	void Start(){
 		//string IMEI = TM.Call<string>("getDeviceId");
-		string IMEI = "359279065155027";
+		//string IMEI = "359279065155027";
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		MouseInput ();
 //#if UNITY_EDITOR_OSX || UNITY_STANDALONE
 //		MouseInput();	//control by mouse
 //#elif UNITY_ANDROID 
 //		MobileInput(); //control by finger
 //#endif
-//	}
-//	void MouseInput(){
-//		if (Input.GetKeyDown("space")) {
-//		}
-//	}
+	}
+	void MouseInput(){
+		if (Input.GetKeyDown("space")) {
+			StartCoroutine(login());
+		}
+	}
 //	void MobileInput(){
-	}	
+//	}
+	IEnumerator login(){
+		WWW test_connect = new WWW ("128.199.83.67/APPgame/backside/php/connect.php");
+		yield return test_connect;
+
+		if (test_connect.error != null) {
+			print ("Connection_error by: " + test_connect.error);
+		} else {
+			IDictionary jsonDictionary = (IDictionary)Json.Deserialize(test_connect.text);
+			print (1);
+			print (jsonDictionary);
+			IList jsonList = (IList) jsonDictionary["Objects"];
+
+			foreach(IDictionary data in jsonList){
+				print(string.Format("data: {0} : {1}", data[1], data[2]));
+			}
+			yield break;
+		}
+	}
 }
 
