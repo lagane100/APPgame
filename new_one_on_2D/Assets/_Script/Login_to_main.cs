640d440129d493fc712a506e17a14f6f9481361c
lagane100 <lagane100@gmail.com>
2015-11-30 08:31:57 +0800

    add login json Success

--- new_one_on_2D/Assets/_Script/Login_to_main.cs
+++ new_one_on_2D/Assets/_Script/Login_to_main.cs
@@ -7,7 +7,7 @@ using MiniJSON;
 public class Login_to_main : MonoBehaviour {
 
 	//private AndroidJavaObject TM = new AndroidJavaObject("android.telephony.TelephonyManager");
-	public string IMEI = "359279065155027";
+	private string IMEI = "359279065155027";
 
 	// Start is called once per script
 	void Start(){
@@ -22,32 +22,39 @@ public class Login_to_main : MonoBehaviour {
 		if (Input.GetKeyDown("space")) {
 			WWWForm form = new WWWForm();
 			form.AddField("IMEI", IMEI);
-			WWW test_connect = new WWW ("128.199.83.67/APPgame/backside/php/test.php",form);
-			StartCoroutine(login(test_connect));
+			WWW login_data = new WWW ("128.199.83.67/APPgame/backside/php/login.php",form);
+			StartCoroutine(login(login_data));
 		}
 	}
 
 	//login coroutine
-	IEnumerator login(WWW test_connect){
-		yield return test_connect;
-		if (test_connect.error != null) {
-			print ("Connection_error by: " + test_connect.error);
+	IEnumerator login(WWW login_data){
+		yield return login_data;
+		if (login_data.error != null) {
+			print ("Connection_error by: " + login_data.error);
 			yield break;
-		} else {
-			//var results = Json.Deserialize(test_connect.text) as Dictionary<string,object>;
-
-			IDictionary search = (IDictionary)Json.Deserialize(test_connect.text);
-			print (search["UID"]);
-			//IList tweets = (IList) search["results"];
-			//Debug.Log(search);
-			//Debug.Log(tweets);
-
-			//foreach (IDictionary tweet in tweets) {
-			//	Debug.Log(string.Format("tweet: {0} : {1}", tweet["0"], tweet["UID"]));
-			//}
+		}
+		else if(login_data.text.Equals("need new account")){
+			WWW register_data = new WWW("128.199.83.67/APPgame/backside/php/register.php");
+			yield return StartCoroutine(register(register_data));
+		}
+		else {
+			//IDictionary<string,object> results = (IDictionary<string,object>)Json.Deserialize(login_data.text);
+			Debug.Log(login_data.text);
+			/*IList tweets = (IList) search["results"];
+			 * 
+			foreach (IDictionary tweet in tweets) {
+				Debug.Log(string.Format("tweet: {0} : {1}", tweet["0"], tweet["UID"]));
+			}*/
 
 			yield break;
 		}
 	}
+
+	//register coroutine
+	IEnumerator register(WWW register_data){
+		yield return null;
+	}
+
 }
 
