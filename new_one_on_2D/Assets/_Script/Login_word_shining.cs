4128fb680bbb9318796b21c017438a91b8ca1a07
lagane100 <lagane100@gmail.com>
2015-12-03 15:08:24 +0800

    add word shininig

---
+++ new_one_on_2D/Assets/_Script/Login_word_shining.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class Login_word_shining : MonoBehaviour {
+
+	private int timeCount = 0;
+	private bool isShow = false;
+	private GameObject Enter;
+
+	// Use this for initialization
+	void Start () {
+		Enter = GameObject.FindGameObjectWithTag ("press to continue");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCount = timeCount + 1;
+		if (timeCount < 60) {
+			isShow = true;
+		} else if (timeCount < 120) {
+			isShow = false;
+		} else {
+			timeCount = timeCount - 120;
+		}
+		Enter.SetActive (isShow);
+	}
+}
