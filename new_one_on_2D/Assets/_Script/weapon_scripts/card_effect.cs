3156187af6afbbaf031b87074d4f88e35f289203
lagane100 <lagane100@gmail.com>
2015-12-03 21:32:43 +0800

    add Card effect

---
+++ new_one_on_2D/Assets/_Script/weapon_scripts/card_effect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class card_effect : MonoBehaviour {
+
+	private Vector3 biggest_scale = new Vector3(2.0f,2.0f,2.0f);
+	private int Counter = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localScale == biggest_scale) {
+			Counter++;
+		}
+		if (Counter == 0) {
+			transform.localScale += new Vector3(0.05f,0.05f,0.05f);
+		}
+		if (Counter == 1) {
+			transform.localScale -= new Vector3(0.05f,0.05f,0.05f);
+		}
+		if (Counter == 1 && transform.localScale == new Vector3 (1.0f, 1.0f, 1.0f)) {
+			this.enabled = false;
+		}
+	}
+}
