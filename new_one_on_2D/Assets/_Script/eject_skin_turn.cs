053635405660904b22bdb4a66fcb0b13f31f9808
lagane100 <lagane100@gmail.com>
2015-12-10 18:07:01 +0800

    finish half of end script

---
+++ new_one_on_2D/Assets/_Script/eject_skin_turn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eject_skin_turn : MonoBehaviour {
+
+	private GameObject end_turn_prefab;
+	// Use this for initialization
+	void Start () {
+		end_turn_prefab = GameObject.FindGameObjectWithTag("skipTurn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTouchDown(){
+		if (end_turn_prefab) {
+			Destroy (end_turn_prefab);
+		}
+	}
+}
--- new_one_on_2D/Assets/_Script/end.cs
+++ new_one_on_2D/Assets/_Script/end.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class end : MonoBehaviour {
+	
+	public GameObject end_turn_prefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	//end the player turn
+	void OnTouchDown(){
+		Instantiate (end_turn_prefab, new Vector3 (0.0f, 0.0f, 0.0f), new Quaternion ());
+	}	
 }
