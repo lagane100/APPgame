62794466cdd126348fa9ff0a9a21d31139aa2d56
lagane100 <lagane100@gmail.com>
2015-12-13 20:29:07 +0800

    change personal data

---
+++ new_one_on_2D/Assets/_Script/open_data.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class open_data : MonoBehaviour {
+
+	public GameObject data_info;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTouchDown(){
+		Instantiate (data_info, new Vector3 (0.0f, 0.0f, 0.0f), new Quaternion ());
+	}
+}
