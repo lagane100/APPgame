68dace3031f8ab3e4f27bb4001915c4c2f8c58bb
lagane100 <lagane100@gmail.com>
2015-12-14 15:49:31 +0800

    personal data change back

---
+++ new_one_on_2D/Assets/_Script/let_mesh_float.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class let_mesh_float : MonoBehaviour {
+
+	void Awake(){
+		GameObject.Find ("exp_main").GetComponent<SpriteRenderer> ().sortingOrder = 3;
+		GameObject.Find ("level_main").GetComponent<SpriteRenderer> ().sortingOrder = 3;
+	}
+}
+	
