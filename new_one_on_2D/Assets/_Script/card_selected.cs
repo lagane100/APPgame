a7fe882dfb304d756c69a6f25ddff40e3482a6e2
lagane100 <lagane100@gmail.com>
2015-12-14 16:05:10 +0800

    chatroom

--- new_one_on_2D/Assets/_Script/card_selected.cs
+++ new_one_on_2D/Assets/_Script/card_selected.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class card_selected : MonoBehaviour {
 
+	private Vector3 origin_size = new Vector3 (0.75f, 0.75f, 0.75f);
+	private Vector3 selected_size = new Vector3 (1.0f, 1.0f, 1.0f);
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +17,11 @@ public class card_selected : MonoBehaviour {
 	}
 
 	void OnTouchDown(){
-		transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+		if (transform.localScale.x == origin_size.x) {
+			transform.localScale = selected_size;
+		}
+		else if (transform.localScale.x == selected_size.x) {
+			transform.localScale = origin_size;
+		}
 	}
 }
--- new_one_on_2D/Assets/_Script/open_chatting_room.cs
+++ new_one_on_2D/Assets/_Script/open_chatting_room.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class open_chatting_room : MonoBehaviour {
 
+	private GameObject chatroom;
 	// Use this for initialization
 	void Start () {
-	
+		chatroom = GameObject.FindGameObjectWithTag("chatroom");
+		chatroom.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTouchDown(){
+		chatroom.SetActive (!chatroom.activeSelf);
 	}
 }
